c446ed7e327366ac624f1d8ad1bc53ee5594982d
DINESH MANI <dineshmech0397@gmail.com>
2026-08-21 08:43:20 +0530

    Change API_URL to relative path for employees

--- frontend/src/components/EmployeeForm.js
+++ frontend/src/components/EmployeeForm.js
@@ -5,7 +5,7 @@ function EmployeeForm({
   setSelectedEmployee,
   refreshTable,
 }) {
-  const API_URL = "http://employee-backend-alb-588522014.us-east-1.elb.amazonaws.com/employees";
+  const API_URL = "/employees";
 
   const [employeeId, setEmployeeId] = useState("");
   const [fullName, setFullName] = useState("");
